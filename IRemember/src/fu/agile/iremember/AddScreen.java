42bd8da6698a1161e9023b366354200b1fc32aba
hungdh <hundh@live.com>
2014-08-18 09:56:15 +0700

    Add screen...

--- IRemember/src/fu/agile/iremember/AddScreen.java
+++ IRemember/src/fu/agile/iremember/AddScreen.java
@@ -24,17 +24,19 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddScreen extends Activity implements OnClickListener , LocationListener {
 
 	//Button Declare
-	private Button btAddPhto;
-	private Button create;
-	private Button btAddVideo;
-	private Button btAddAudio;
-	private Button btGetLocation;
+	private ImageButton btAddPhto;
+	private ImageButton create;
+	private ImageButton btAddVideo;
+	private ImageButton btAddImage;
+	private ImageButton btAddAudio;
+	private ImageButton btGetLocation;
 	
 	//Edit Text Declare
 	private EditText etImaegName;
@@ -70,20 +72,22 @@ public class AddScreen extends Activity implements OnClickListener , LocationLis
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.fragment_add_screen);
+		setContentView(R.layout.activity_add_event);
 		time = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
-		btAddPhto = (Button) findViewById(R.id.btAddPhoto);
+		btAddPhto = (ImageButton) findViewById(R.id.btAddTitle);
 		btAddPhto.setOnClickListener(this);
-		btAddVideo = (Button)findViewById(R.id.btAddVideo1);
+		btAddVideo = (ImageButton)findViewById(R.id.btAddVideo);
 		btAddVideo.setOnClickListener(this);
-		btAddAudio = (Button) findViewById(R.id.btRecorder);
+		btAddAudio = (ImageButton) findViewById(R.id.btAddAudio);
 		btAddAudio.setOnClickListener(this);
-		btGetLocation = (Button) findViewById(R.id.btGetlocation);
+		btAddImage = (ImageButton) findViewById(R.id.btAddImage);
+		btAddImage.setOnClickListener(this);
+		btGetLocation = (ImageButton) findViewById(R.id.btGetlocation);
 		btGetLocation.setOnClickListener(this);
 		etImaegName = (EditText) findViewById(R.id.etImageName);
 		etTitle = (EditText) findViewById(R.id.etTitle);
 		etBody = (EditText) findViewById(R.id.etBody);
-		create = (Button) findViewById(R.id.btCreate);
+		create = (ImageButton) findViewById(R.id.btCreate);
 		create.setOnClickListener(this);
 		TimeView = (TextView)findViewById(R.id.twSetTime);
 		TimeView.setOnClickListener(this);
@@ -91,102 +95,102 @@ public class AddScreen extends Activity implements OnClickListener , LocationLis
 		latituteField = (TextView)findViewById(R.id.twLatitude);
 		db = new DataBase(this);
 		
-
-		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		Criteria criteria = new Criteria();
-		provider = locationManager.getBestProvider(criteria, false);
-		latitute = "0";
-	    longitude = "0";
-	    // Initialize the location fields
-	    location = locationManager.getLastKnownLocation(provider);
-	    if (location != null) {
-		      System.out.println("Provider " + provider + " has been selected.");
-		      onLocationChanged(location);
-		    } else {
-		    	latituteField.setText("Location not available");
-		    	longitudeField.setText("Location not available");
-		    }
-	}
-	
-	
-	  @Override
-	  protected void onResume() {
-	    super.onResume();
-	    	locationManager.requestLocationUpdates(provider, 400, 1, this);
-	  }
-
-	  /* Remove the locationlistener updates when Activity is paused */
-	  @Override
-	  protected void onPause() {
-	    super.onPause();
-	    	locationManager.removeUpdates(this);
-	  }
-
-	  @Override
-	  public void onLocationChanged(Location location) {
-			int lat = (int) (location.getLatitude());
-		    int lng = (int) (location.getLongitude());
-		    latitute = String.valueOf(lat);
-		    longitude = String.valueOf(lng);
-	  }
-
-	  @Override
-	  public void onStatusChanged(String provider, int status, Bundle extras) {
-	    // TODO Auto-generated method stub
-
-	  }
-
-	  @Override
-	  public void onProviderEnabled(String provider) {
-	    Toast.makeText(this, "Enabled new provider " + provider,
-	        Toast.LENGTH_SHORT).show();
-
-	  }
-
-	  @Override
-	  public void onProviderDisabled(String provider) {
-	    Toast.makeText(this, "Disabled provider " + provider,
-	        Toast.LENGTH_SHORT).show();
-	  }
-	
-	
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if(requestCode == TAKE_PICTURE_CODE) {
-			if(resultCode == RESULT_OK){      
-		    	 imagePath = data.getStringExtra("resultOfPhoto");
-		    	 etImaegName.setText(new File(imagePath).getName());
-		     }
-		} 
-		if(requestCode == TAKE_VIDEO_CODE) {
-			if(resultCode == RESULT_OK){      
-		    	 videoPath = data.getStringExtra("resultOfVideo");
-		     }
-		} 
-		if(requestCode == TAKE_AUDIO_RECORDER) {			
-				audioPath = data.getStringExtra("AudioPath");
-				Log.d(tag, audioPath.toString());
-		}
-	} 
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.add_screen, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
+//
+//		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+//		Criteria criteria = new Criteria();
+//		provider = locationManager.getBestProvider(criteria, false);
+//		latitute = "0";
+//	    longitude = "0";
+//	    // Initialize the location fields
+//	    location = locationManager.getLastKnownLocation(provider);
+//	    if (location != null) {
+//		      System.out.println("Provider " + provider + " has been selected.");
+//		      onLocationChanged(location);
+//		    } else {
+//		    	latituteField.setText("Location not available");
+//		    	longitudeField.setText("Location not available");
+//		    }
+//	}
+//	
+//	
+//	  @Override
+//	  protected void onResume() {
+//	    super.onResume();
+//	    	locationManager.requestLocationUpdates(provider, 400, 1, this);
+//	  }
+//
+//	  /* Remove the locationlistener updates when Activity is paused */
+//	  @Override
+//	  protected void onPause() {
+//	    super.onPause();
+//	    	locationManager.removeUpdates(this);
+//	  }
+//
+//	  @Override
+//	  public void onLocationChanged(Location location) {
+//			int lat = (int) (location.getLatitude());
+//		    int lng = (int) (location.getLongitude());
+//		    latitute = String.valueOf(lat);
+//		    longitude = String.valueOf(lng);
+//	  }
+//
+//	  @Override
+//	  public void onStatusChanged(String provider, int status, Bundle extras) {
+//	    // TODO Auto-generated method stub
+//
+//	  }
+//
+//	  @Override
+//	  public void onProviderEnabled(String provider) {
+//	    Toast.makeText(this, "Enabled new provider " + provider,
+//	        Toast.LENGTH_SHORT).show();
+//
+//	  }
+//
+//	  @Override
+//	  public void onProviderDisabled(String provider) {
+//	    Toast.makeText(this, "Disabled provider " + provider,
+//	        Toast.LENGTH_SHORT).show();
+//	  }
+//	
+//	
+//	@Override
+//	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//		if(requestCode == TAKE_PICTURE_CODE) {
+//			if(resultCode == RESULT_OK){      
+//		    	 imagePath = data.getStringExtra("resultOfPhoto");
+//		    	 etImaegName.setText(new File(imagePath).getName());
+//		     }
+//		} 
+//		if(requestCode == TAKE_VIDEO_CODE) {
+//			if(resultCode == RESULT_OK){      
+//		    	 videoPath = data.getStringExtra("resultOfVideo");
+//		     }
+//		} 
+//		if(requestCode == TAKE_AUDIO_RECORDER) {			
+//				audioPath = data.getStringExtra("AudioPath");
+//				Log.d(tag, audioPath.toString());
+//		}
+//	} 
+//
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.add_screen, menu);
+//		return true;
+//	}
+//
+//	@Override
+//	public boolean onOptionsItemSelected(MenuItem item) {
+//		// Handle action bar item clicks here. The action bar will
+//		// automatically handle clicks on the Home/Up button, so long
+//		// as you specify a parent activity in AndroidManifest.xml.
+//		int id = item.getItemId();
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
+//		return super.onOptionsItemSelected(item);
 	}
 
 
@@ -194,15 +198,16 @@ public class AddScreen extends Activity implements OnClickListener , LocationLis
 	public void onClick(View v) {
 		
 		switch(v.getId()) {
-			case R.id.btAddPhoto : {
+			case R.id.btAddImage : {
+				findViewById(R.id.imageAddLayout).setVisibility(View.VISIBLE);
 				openNewGameDialog();
 			} break;
-			case R.id.btAddVideo1 : {
+			case R.id.btAddVideo : {
 				Intent intent = new Intent(AddScreen.this,PhotoCapture.class);
 				intent.putExtra("Video", 3);
 				startActivityForResult(intent, TAKE_VIDEO_CODE);
 			} break;
-			case R.id.btRecorder : {
+			case R.id.btAddAudio : {
 				Intent intent = new Intent(AddScreen.this,Recording.class);
 				startActivityForResult(intent, TAKE_AUDIO_RECORDER);
 				break;
@@ -259,5 +264,33 @@ public class AddScreen extends Activity implements OnClickListener , LocationLis
 		Intent intent = new Intent(AddScreen.this,PhotoCapture.class);
 		intent.putExtra("key", which);
 		startActivityForResult(intent, TAKE_PICTURE_CODE);
+	}
+
+
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
 	}
 }
